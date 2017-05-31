7050fa36fe3f53c04dc5a7582c8010c8925f6fff
chris <kristjan.kosic@gmail.com>
2017-05-31 22:49:20 +0200

    gui console demo app building

--- main/arkcli.go
+++ main/arkcli.go
@@ -10,47 +10,59 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
 
 	"github.com/fatih/color"
+	"github.com/spf13/viper"
 )
 
 var arkclient = core.NewArkClient(nil)
+var reader = bufio.NewReader(os.Stdin)
+
 var passphrase1 = ""
 var passphrase2 = ""
 
-func calculcateVoteRatio() core.TransactionPayload {
-	arkapi := core.NewArkClient(nil)
+var errorlog *os.File
+var logger *log.Logger
 
-	deleKey := "027acdf24b004a7b1e6be2adf746e3233ce034dbb7e83d4a900f367efc4abd0f21"
-	if core.EnvironmentParams.Network.Type == core.DEVNET {
-		deleKey = "02bcfa0951a92e7876db1fb71996a853b57f996972ed059a950d910f7d541706c9 "
+func init() {
+	errorlog, err := os.OpenFile("ark-goclient.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		os.Exit(1)
 	}
 
-	params := core.DelegateQueryParams{PublicKey: deleKey}
+	logger = log.New(errorlog, "applog: ", log.Lshortfile|log.LstdFlags)
+}
 
-	votersEarnings := arkapi.CalculateVotersProfit(params, 0.70)
+//CalculateVotersProfit based on parameters in config.toml
+func calculcateVoteRatio() core.TransactionPayload {
+
+	params := core.DelegateQueryParams{PublicKey: viper.GetString("delegate.pubkey")}
+	votersEarnings := arkclient.CalculateVotersProfit(params, viper.GetFloat64("voters.shareratio"))
+
+	shareRatioStr := strconv.FormatFloat(viper.GetFloat64("voters.shareratio")*100, 'f', -1, 64) + "%"
 
 	var payload core.TransactionPayload
-
-	//log.Println(t.Name(), "Success", votersEarnings)
-	fmt.Print("Enter text: ")
-	var input string
-	fmt.Scanln(&input)
-	fmt.Print(input)
 
 	sumEarned := 0.0
 	sumRatio := 0.0
 	sumShareEarned := 0.0
 	feeAmount := float64(len(votersEarnings)) * (float64(core.EnvironmentParams.Fees.Send) / core.SATOSHI)
+	color.Set(color.FgHiGreen)
+	fmt.Println(fmt.Sprintf("|%34s|%18s|%8s|%17s|%17s|%6s|", "Address", "Balance", "Weight", "Reward-100%", "Reward-"+shareRatioStr, "Hours"))
+	color.Set(color.FgCyan)
 	for _, element := range votersEarnings {
-		log.Println(fmt.Sprintf("|%s|%15.8f|%15.8f|%15.8f|%15.8f|%4d|%25d|",
+		s := fmt.Sprintf("|%s|%18.8f|%8.4f|%15.8f A|%15.8f A|%6d|",
 			element.Address,
 			element.VoteWeight,
 			element.VoteWeightShare,
 			element.EarnedAmount100,
 			element.EarnedAmountXX,
-			element.VoteDuration,
-			int(element.EarnedAmountXX*core.SATOSHI)))
+			element.VoteDuration)
+
+		fmt.Println(s)
+		logger.Println(s)
 
 		sumEarned += element.EarnedAmount100
 		sumShareEarned += element.EarnedAmountXX
@@ -59,15 +71,16 @@ func calculcateVoteRatio() core.TransactionPayload {
 		//transaction parameters
 		tx := core.CreateTransaction(element.Address,
 			int64(element.EarnedAmountXX*core.SATOSHI),
-			"chris: 1st profit sharing payment... |tx made with ark-go",
+			viper.GetString("voters.tx_description"),
 			"",
 			"")
 
 		payload.Transactions = append(payload.Transactions, tx)
 
 	}
-	log.Println("Full forged amount: ", sumEarned, "Ratio calc check sum: ", sumRatio, "Amount to voters: ", sumShareEarned, "Ratio shared: ", float64(sumShareEarned)/float64(sumEarned), "Lottery:", int64((sumEarned-sumShareEarned-feeAmount)*core.SATOSHI))
-	log.Println(fmt.Sprintf("Payment fees: %2.2f", feeAmount))
+
+	logger.Println("Full forged amount: ", sumEarned, "Ratio calc check sum: ", sumRatio, "Amount to voters: ", sumShareEarned, "Ratio shared: ", float64(sumShareEarned)/float64(sumEarned), "Lottery:", int64((sumEarned-sumShareEarned-feeAmount)*core.SATOSHI))
+	logger.Println(fmt.Sprintf("Payment fees: %2.2f", feeAmount))
 
 	tx := core.CreateTransaction("ANqeL7CP2som7q9NFbRuaUc5WUnwYkSbFY",
 		int64((sumEarned-sumShareEarned-feeAmount)*core.SATOSHI),
@@ -76,6 +89,8 @@ func calculcateVoteRatio() core.TransactionPayload {
 		"")
 
 	payload.Transactions = append(payload.Transactions, tx)
+
+	pause()
 
 	return payload
 
@@ -99,7 +114,7 @@ func readAccountData() {
 
 	accountResp, _, _ := arkclient.GetAccount(core.AccountQueryParams{Address: key.PublicKey.Address()})
 	if !accountResp.Success {
-		log.Println("Error getting account data for address", key.PublicKey.Address())
+		logger.Println("Error getting account data for address", key.PublicKey.Address())
 		return
 	}
 
@@ -107,68 +122,77 @@ func readAccountData() {
 		fmt.Print("Enter second account passphrase: ")
 		passphrase2, _ = reader.ReadString('\n')
 	}
+
 }
 
 //////////////////////////////////////////////////////////////////////////////
-
-var clear map[string]func() //create a map for storing clear funcs
-
-func init() {
-	clear = make(map[string]func()) //Initialize it
-	clear["linux"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-	clear["windows"] = func() {
-		cmd := exec.Command("cls") //Windows example it is untested, but I think its working
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
+//GUI RELATED STUFF
+func pause() {
+	fmt.Print("Press any key to return to the menu... ")
+	//bufio.NewReader(os.Stdin).ReadBytes('\n')
+	reader.ReadString('\n')
 }
 
-func CallClear() {
-	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-	if ok {                          //if we defined a clear func for that platform:
-		value() //we execute it
-	} else { //unsupported platform
-		panic("Your platform is unsupported! I can't clear terminal screen :(")
+func clearScreen() {
+	cmd := exec.Command("clear")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
 	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
 }
 
 func printMenu() {
-	CallClear()
+
 	color.Set(color.FgHiGreen)
-	dat, _ := ioutil.ReadFile("banner1.txt")
+	dat, _ := ioutil.ReadFile("settings/banner5.txt")
 
 	fmt.Print(string(dat))
 	color.Unset()
 
-	fmt.Println("==========================")
-	fmt.Println("1-Display contributors")
-	fmt.Println("2-Link account")
-	fmt.Println("0-Exit")
-	fmt.Println("==========================")
+	color.Set(color.FgHiYellow)
+	fmt.Println("")
+	fmt.Println("\t1-Display contributors")
+	fmt.Println("\t2-Send payments")
+	fmt.Println("\t4-Link account")
+	fmt.Println("\t5-Exit")
+	fmt.Println("")
+	fmt.Print("\tSelect option [1-5]:")
 	color.Unset()
 }
 
 func main() {
+	logger.Println("Ark-golang client starting")
+	clearScreen()
+
+	viper.SetConfigName("config")   // name of config file (without extension)
+	viper.AddConfigPath("settings") // path to look for the config file in
+	viper.AddConfigPath(".")        // optionally look for config in the working directory
+	err := viper.ReadInConfig()     // Find and read the config file
+	if err != nil {                 // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
 	var choice int
 
-	for choice != 9 {
+	for choice != 5 {
+		//pause()
 		printMenu()
 		fmt.Scan(&choice)
 		switch choice {
 		case 1:
+			clearScreen()
+			color.Set(color.FgMagenta)
 			calculcateVoteRatio()
+			color.Unset()
 		case 2:
 
 		case 3:
 
-		case 4:
-			fmt.Println("Bye!")
-		default:
-			fmt.Printf("Are you sure you have chosen properly?")
 		}
 	}
+
+	defer errorlog.Close()
 }
