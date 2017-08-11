f5c8c6cbc67bc9cb6240ad170ff61a815b4b0999
chris <kristjan.kosic@adora-med.com>
2017-08-11 14:03:18 +0200

    filtering peers also by height

--- core/arkenv.go
+++ core/arkenv.go
@@ -118,7 +118,8 @@ func LoadActiveConfiguration(arknetwork ArkNetworkType) string {
 	for i := len(EnvironmentParams.Network.PeerList) - 1; i >= 0; i-- {
 		peer := EnvironmentParams.Network.PeerList[i]
 		// Condition to decide if current element has to be deleted:
-		if peer.Status != "OK" {
+		// Also skipping nodes that are more then 50 blocks off current chain height
+		if peer.Status != "OK" && (peer.Height+50) >= EnvironmentParams.Network.ActivePeer.Height {
 			EnvironmentParams.Network.PeerList = append(EnvironmentParams.Network.PeerList[:i], EnvironmentParams.Network.PeerList[i+1:]...)
 		}
 	}
