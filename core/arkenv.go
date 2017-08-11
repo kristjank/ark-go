2462a8f2ad5248458dc5c1fa9de64b7a55bbb778
chris <kristjan.kosic@adora-med.com>
2017-08-11 15:03:58 +0200

    some more peer cleaning :)

--- core/arkenv.go
+++ core/arkenv.go
@@ -119,8 +119,9 @@ func LoadActiveConfiguration(arknetwork ArkNetworkType) string {
 		peer := EnvironmentParams.Network.PeerList[i]
 		// Condition to decide if current element has to be deleted:
 		// Also skipping nodes that are more then 50 blocks off current chain height
-		if peer.Status != "OK" && (peer.Height+50) >= EnvironmentParams.Network.ActivePeer.Height {
+		if peer.Status != "OK" || (peer.Height+40) <= EnvironmentParams.Network.ActivePeer.Height {
 			EnvironmentParams.Network.PeerList = append(EnvironmentParams.Network.PeerList[:i], EnvironmentParams.Network.PeerList[i+1:]...)
+			log.Println("Removing peer", peer.IP, peer.Status, peer.Height)
 		}
 	}
 	return "http://" + selectedPeer
