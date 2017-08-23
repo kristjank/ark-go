e7ce2ae5b2242b915d48b39fb87906332720c391
chris <kristjan.kosic@gmail.com>
2017-08-23 13:16:16 +0200

    Added additional peer optimization based on height (removing peers lover than 10 blocks from maxheight) + added additional test method

--- core/network_test.go
+++ core/network_test.go
@@ -25,3 +25,16 @@ func TestSwitchPeer(t *testing.T) {
 	log.Println(arkapi.GetActivePeer())
 	log.Println(t.Name(), "Success")
 }
+
+func TestSwitchNetwork(t *testing.T) {
+	arkapi := NewArkClient(nil)
+
+	for i := 0; i < 5; i++ {
+		arkapi = arkapi.SetActiveConfiguration(MAINNET)
+		log.Println("Selected: ", arkapi.GetActivePeer())
+	}
+
+	arkapi = arkapi.SwitchPeer()
+	log.Println(arkapi.GetActivePeer())
+	log.Println(t.Name(), "Success")
+}
