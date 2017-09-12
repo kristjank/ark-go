cbc84a2954ba965f87dd68e0618f517f5fb47f93
chris <kristjan.kosic@adora-med.com>
2017-09-12 21:09:50 +0200

    sample config

--- cmd/arkgoserver/arkgo-server.go
+++ cmd/arkgoserver/arkgo-server.go
@@ -85,7 +85,6 @@ func loadConfig() {
 
 	viper.SetDefault("server.address", "0.0.0.0")
 	viper.SetDefault("server.port", 54000)
-	viper.SetDefault("server.version", "0.1.0")
 	viper.SetDefault("server.dbfilename", "payments.db")
 }
 
