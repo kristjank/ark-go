a3a844260f716b6fb39c52e05dcb3ddf8c48cb0f
chris <kristjan.kosic@gmail.com>
2017-10-03 09:24:55 +0200

    added usb release to make

--- cmd/arkgopool/make.bat
+++ cmd/arkgopool/make.bat
@@ -11,7 +11,7 @@ if not exist "dist" mkdir dist
 cd dist
 mkdir linux
 cd linux
-move ..\..\arkgopool .
+copy ..\..\arkgopool .
 mkdir settings
 copy ..\..\settings\banner.txt settings
 copy ..\..\settings\sample.config.toml settings
@@ -21,16 +21,28 @@ cd ..
 REM windows
 if not exist "windows" mkdir windows
 cd windows
-move ..\..\arkgopool.exe .
+copy ..\..\arkgopool.exe .
 mkdir settings
 copy ..\..\settings\banner.txt settings
 copy ..\..\settings\sample.config.toml settings
 mkdir log
+cd ..
+
+REM UsbKeyRelease
+if not exist "usb" mkdir usb
+cd usb
+move ..\..\arkgopool.exe .
+move ..\..\arkgopool .
+mkdir settings
+copy ..\..\settings\banner.txt settings
+copy ..\..\settings\sample.config.toml settings
+mkdir log
+cd ..
 
 REM create archive
-cd ..
 for /d %%X in (*) do "c:\Program Files\7-Zip\7z.exe" a -mx "%%X.zip" "%%X\*"
 
 move linux.zip ARKGO-LinuxRelease.zip
 move windows.zip ARKGO-WindowsRelease.zip
+move usb.zip ARKGO-UsbKeyRelease.zip
 
