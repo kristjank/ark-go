816ef08714fa3e6e287704b41bfcd1e2ab447a60
chris <kristjan.kosic@gmail.com>
2017-10-03 09:04:11 +0200

    build script

--- cmd/arkgopool/make.bat
+++ cmd/arkgopool/make.bat
@@ -1,7 +1,29 @@
+
 go get golang.org/x/sys/unix
 set GOARCH=amd64
 set GOOS=linux
-go build .\arkgopool.go .\database.go .\helpers.go .\payouts.go
+go build 
 
 set GOOS=windows
-go build .\arkgopool.go .\database.go .\helpers.go .\payouts.go
+go build 
+
+REM linux
+if not exist "dist" mkdir dist
+cd dist
+mkdir linux
+cd linux
+move ..\..\arkgopool .
+mkdir settings
+copy ..\..\settings\banner.txt settings
+copy ..\..\settings\sample.config.toml settings
+cd ..
+
+REM windows
+if not exist "windows" mkdir windows
+cd windows
+move ..\..\arkgopool.exe .
+mkdir settings
+copy ..\..\settings\banner.txt settings
+copy ..\..\settings\sample.config.toml settings
+mkdir log
+
