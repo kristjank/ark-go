3246130c511ada8e78bccc002c6d924a91b25710
chris <kristjan.kosic@gmail.com>
2017-11-03 22:26:13 +0100

    arkgotest project

--- cmd/arkgotest/arkgotest.go
+++ cmd/arkgotest/arkgotest.go
@@ -1,11 +1,59 @@
 package main
 
 import (
-	"log"
+	"io"
+	"os"
 	"time"
 
 	"github.com/kristjank/ark-go/core"
+	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	initLogger()
+	loadConfig()
+
+}
+func initLogger() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+
+	// You could set this to any `io.Writer` such as a file
+	file, err := os.OpenFile("log/arkgotest.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(io.MultiWriter(file))
+	} else {
+		log.Error("Failed to log to file, using default stderr")
+	}
+
+}
+
+func loadConfig() {
+	viper.AddConfigPath("cfg")  // path to look for the config file in
+	err := viper.ReadInConfig() // Find and read the config file
+
+	if err != nil {
+		log.Info("No productive config found - loading sample")
+		// try to load sample config
+		viper.SetConfigName("sample.config")
+		viper.AddConfigPath("cfg")
+		err := viper.ReadInConfig()
+
+		if err != nil { // Handle errors reading the config file
+			log.Fatal("No configuration file loaded - using defaults")
+		}
+	}
+
+	viper.SetDefault("env.txPerPayload", 250)
+	viper.SetDefault("env.txMultiBroadCast", 1)
+	viper.SetDefault("env.dbfilename", "db/testlog.db")
+}
+
+func checkTx4Confirmations() {
+
+}
 
 func main() {
 	arkapi := core.NewArkClient(nil)
@@ -20,7 +68,7 @@ func main() {
 	}
 	t0 := time.Now()
 
-	for xx := 0; xx < 250; xx++ {
+	for xx := 0; xx < 1; xx++ {
 		arkapi = arkapi.SetActiveConfiguration(core.DEVNET)
 
 		var payload core.TransactionPayload
@@ -49,4 +97,5 @@ func main() {
 
 	t1 := time.Now()
 	log.Printf("The call took %v to run.\n", t1.Sub(t0))
+
 }
