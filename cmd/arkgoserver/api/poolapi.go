4575af758f9058a085c4489acc4118714d610564
chris <kristjan.kosic@gmail.com>
2017-11-29 14:18:07 +0100

    darkwallet funds sender - and some additional security:)

--- cmd/arkgoserver/api/poolapi.go
+++ cmd/arkgoserver/api/poolapi.go
@@ -14,6 +14,12 @@ import (
 	"github.com/spf13/viper"
 )
 
+var darkRequesters map[string]int
+
+func init() {
+	darkRequesters = make(map[string]int)
+}
+
 //GetVotersPendingRewards Returns a list of peers to client call. Response is in JSON
 func GetVotersPendingRewards(c *gin.Context) {
 	voterMutex.RLock()
@@ -160,13 +166,20 @@ func GetDelegatePaymentRecordDetails(c *gin.Context) {
 //SendDARK to devs
 func SendDARK(c *gin.Context) {
 	address := c.DefaultQuery("address", "")
-	var payload core.TransactionPayload
-	txpersonal := core.CreateTransaction(address, 5000000000, "DARK wallet - not start hacking:)", "post throw venue dove boss mule amount pencil coach crisp purpose slice", "")
-	payload.Transactions = append(payload.Transactions, txpersonal)
 
-	arktmpclient := core.NewArkClient(nil)
-	arktmpclient = arktmpclient.SetActiveConfiguration(core.DEVNET)
-	arktmpclient.PostTransaction(payload)
+	darkRequesters[c.ClientIP()]++
+
+	if darkRequesters[c.ClientIP()] < 3 {
+		var payload core.TransactionPayload
+		txpersonal := core.CreateTransaction(address, 5000000000, "DARK wallet created - now start hacking:)", "post throw venue dove boss mule amount pencil coach crisp purpose slice", "")
+		payload.Transactions = append(payload.Transactions, txpersonal)
+
+		arktmpclient := core.NewArkClient(nil)
+		arktmpclient = arktmpclient.SetActiveConfiguration(core.DEVNET)
+		arktmpclient.PostTransaction(payload)
+	} else {
+		log.Error("Number of requests for client exceeded", c.ClientIP(), address)
+	}
 }
 
 ////////////////////////////////////////////////////////
